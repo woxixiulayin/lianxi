7a878e126a145ac931475e231a0b76b5a7029640
woxixiulayin <416883813@qq.com>
2014-07-23 16:27:19 +0800

    更改2.c文件

--- 2.c
+++ 2.c
@@ -9,6 +9,24 @@ int rat(int n)
 int main()
 {
 	int n = 0;
+	int a1=1;
+	int i =0;
+	int a2 = 1;
+	int tmp;
+	for(i = 0;i<20;i++)
+	{
+if(i>2)	
+{
+tmp = a1;
+a1 = a1+a2;
+a2 = tmp;
+printf("%d\n",a1);
+}
+else printf("1\n");
+	
+
+	}
+
 	printf("输入月数:");
 	scanf("%d", &n);
 	printf("兔子数目是%d\n", rat(n));
