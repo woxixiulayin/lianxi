74ac932fa2b8c868f4c51f997f7175d5577c6488
woxixiulayin <416883813@qq.com>
2014-07-28 17:59:06 +0800

    90度翻转输出数组

---
+++ 6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int a[4][4] = {0};
+
+	int i,j;
+	
+	for(i = 0;i < 16;i++)
+	{
+		a[i] = rand()%100;
+	}
+	
+	for(i = 0;i < 4;i++)
+	{
+		
+	}
+}
