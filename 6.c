675f9b4b36cface07d0ce8dbb300fd1ddd5501ea
woxixiulayin <416883813@qq.com>
2014-07-29 08:57:48 +0800

    翻转90度输出

--- 6.c
+++ 6.c
@@ -7,13 +7,24 @@ int main()
 
 	int i,j;
 	
-	for(i = 0;i < 16;i++)
-	{
-		a[i] = rand()%100;
-	}
-	
 	for(i = 0;i < 4;i++)
 	{
-		
+		for(j =0;j<4;j++)
+		{
+			a[i][j] = rand()%100;
+			printf("%-4d",a[i][j]);
+		}
+		printf("\n");
 	}
+	
+printf("\n");
+	for(j = 3;j>=0 ;j--)
+	{
+		for(i = 0;i<4;i++)	
+		{
+			printf("%-4d",a[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
 }
